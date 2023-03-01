290946779c9654ca4751b1dd32b0b841122851b6
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2023-03-01 17:32:44 -0300

    Clase 3 ej 1
    Creado modulo para el primer ejercicio de la clase 3

---
+++ ArgProgClases/Clase3-1/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    /*1. Utilizando solo tipos primitivos, String (solo usar método length), vectores,
+    iteraciones simples y condicionales, genere una clase por ejercicio que posea los
+    siguientes métodos:
+    a. Dado un String y una letra, que cuente la cantidad de apariciones de la letra en
+    el String
+    b. Dados 3 números y un orden (ascendente o decreciente) que ordene los
+    mismos y los retorne en un vector de 3
+    c. dado un vector de números, y un número X, que sume todos los números > X y
+    retorne el resultado
+
+    2. Genere una clase que tenga los métodos para realizar la codificación y decodificación
+    de un string, dado un número de desplazamiento.
+    ● Por ejemplo, con desplazo de 1:
+    “hola que tal” -> “ipmbarvfaubm”
+    h -> i
+    o -> p
+    ● con desplazo de 2
+    “hola que tal” -> “jqncbswgbvcn”
+    h -> j
+    o -> q */
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
