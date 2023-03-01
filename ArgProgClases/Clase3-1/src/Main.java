113b388eacb621aa243287a9b3705681ad0e3d70
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2023-03-01 18:22:19 -0300

    clase 3-1 a
    terminado ejercicio a. b incompleto

--- ArgProgClases/Clase3-1/src/Main.java
+++ ArgProgClases/Clase3-1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     /*1. Utilizando solo tipos primitivos, String (solo usar método length), vectores,
     iteraciones simples y condicionales, genere una clase por ejercicio que posea los
@@ -7,20 +9,34 @@ public class Main {
     b. Dados 3 números y un orden (ascendente o decreciente) que ordene los
     mismos y los retorne en un vector de 3
     c. dado un vector de números, y un número X, que sume todos los números > X y
-    retorne el resultado
-
-    2. Genere una clase que tenga los métodos para realizar la codificación y decodificación
-    de un string, dado un número de desplazamiento.
-    ● Por ejemplo, con desplazo de 1:
-    “hola que tal” -> “ipmbarvfaubm”
-    h -> i
-    o -> p
-    ● con desplazo de 2
-    “hola que tal” -> “jqncbswgbvcn”
-    h -> j
-    o -> q */
+    retorne el resultado*/
 
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        Lettercount("a", "te amo julian perrotta");
     }
+
+
+    public static void Lettercount(String letter, String string){
+
+        //No se como comparar chars con strings
+        string = string.toLowerCase();
+        int c = 0;
+        String[] letterlist = new String[string.length()];
+        letterlist = string.split("");
+
+        for (int i=0; i <string.length(); i++){
+            if (letterlist[i].equals(letter)){c++;}
+            }
+        System.out.println("Letter: " + letter+"\nCounts: " + c + "\nin: " + string);
+    }
+     public static void Sort3 (int n1, int n2, int n3, boolean ascendant){
+        int[] numlist = new int[]{n1,n2,n3};
+        if (ascendant){
+            
+        }
+
+     }
+
+
+
 }
---
+++ ArgProgClases/Clase3-2/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    /*2. Genere una clase que tenga los métodos para realizar la codificación y decodificación
+    de un string, dado un número de desplazamiento.
+    ● Por ejemplo, con desplazo de 1:
+            “hola que tal” -> “ipmbarvfaubm”
+    h -> i
+    o -> p
+    ● con desplazo de 2
+            “hola que tal” -> “jqncbswgbvcn”
+    h -> j
+    o -> q */
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
