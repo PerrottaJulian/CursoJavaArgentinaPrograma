8bf5e98bc451f1992c2b69294a2e82b5b46beb9f
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2023-03-02 12:58:26 -0300

    3-1 complete

--- ArgProgClases/Clase3-1/src/Main.java
+++ ArgProgClases/Clase3-1/src/Main.java
@@ -12,9 +12,11 @@ public class Main {
     retorne el resultado*/
 
     public static void main(String[] args) {
-        Lettercount("a", "te amo julian perrotta");
+        Lettercount("a", "TE AMO JULIAN PERROTTA");
+        Sort3(12.45, 12.47, 10.3333, true);
+        double[] arr = new double[]{8, 99.99, 10.01, 2, 3.5, 12, 15.3, 15.7};
+        BiggerThanXAdd(arr, 10);
     }
-
 
     public static void Lettercount(String letter, String string){
 
@@ -29,14 +31,34 @@ public class Main {
             }
         System.out.println("Letter: " + letter+"\nCounts: " + c + "\nin: " + string);
     }
-     public static void Sort3 (int n1, int n2, int n3, boolean ascendant){
-        int[] numlist = new int[]{n1,n2,n3};
+     public static void Sort3 (double n1, double n2, double n3, boolean ascendant){
+        double[] arr = new double[]{n1,n2,n3};
         if (ascendant){
-            
+            Arrays.sort(arr);
+        } else{
+            Arrays.sort(arr);
+            for (int i = 0; i < arr.length/2; i++){
+                double temp = arr[i];
+                arr[i] = arr[arr.length - 1 - i];
+                arr[arr.length - 1 - i] = temp;
+            }
+        }
+
+        for (int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
         }
 
      }
 
+    public static void BiggerThanXAdd(double[] arr,double x){
+        double sum = 0.0;
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] > x){
+                sum += arr[i];
+            }
+        }
+        System.out.println("Suma: " + sum);
 
+    }
 
 }
